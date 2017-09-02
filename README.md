b2914726cbaf556557cc02be7e3961cd2876e161
Lucas Mosele <lucasmosele@gmail.com>
2017-09-02 17:43:38 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# wp-site
+This repo is creating the template for move to wordpress:
+
+In order to run Front-End (Node 6+)
+
+```
+git clone
+npm install
+npm run watch-css
+```
+
+Run PHP Server Locally
+`php -S localhost:9000`
+
---
+++ index.php
@@ -0,0 +1,44 @@
+
+
+<?php echo file_get_contents("./src/modules/_head.php") ?>
+
+<body class="container">
+
+        
+    <header>
+        <nav>
+            <li><a href="#"><h1>Header</h1></a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+        </nav>
+        <button>Button</button>
+    </header>
+
+    <section class="main">
+        <h2>CSS Grid Layout</h2>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+
+        <img src="https://unsplash.it/500/550?image=116">        
+    </section>
+
+    <aside class="sidebar">
+        <h3>Sidebar</h3>
+    </aside>
+
+    <footer>
+        <h3>Footer</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </footer>
+
+
+
+
+	<!-- SCRIPTS -->
+<!-- 	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script> -->
+  <!-- <script type="text/javascript" src="dist/js/remodal.min.js"></script> -->
+	<script type="text/javascript" src="dist/js/main.min.js"></script>
+	
+</body>
+</html>
