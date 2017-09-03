791cc4d64d00cf0db37e6319df6ddcaa7be92ec0
Lucas Mosele <lucasmosele@gmail.com>
2017-09-02 21:19:19 -0400

    markup done

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
   <nav class="home-nav row between-xs">
     <a href="#" class="home-nav-logo col-xs-2">Logo</a>
 
-    <div class="home-nav-logo-list col-xs-8">
-      <a href="#" class="logo-list-link">LOGO 1</a>
-      <a href="#" class="logo-list-link">LOGO 1</a>
-      <a href="#" class="logo-list-link">LOGO 1</a>
-      <a href="#" class="logo-list-link">LOGO 1</a>
-      <a href="#" class="logo-list-link">LOGO 1</a>
-    </div>
+    <ul class="home-nav-logo-list col-lg-6">
+			<li>
+				<a href="#" class="brand-logo dim">
+					<img src="#">
+				</a>
+			</li>
+		</ul>
 
     <div class="home-nav-icons col-xs-2">
       <a href="#" class="icon-link-facebook">FB</a>
@@ -22,8 +22,9 @@
 
   </nav>
 
+  <!-- Splash Header -->
 	<header class="splash row">
-		<div class="splash-container row">
+		<article class="splash-container row">
 			<div class="splash-details col-lg-6">
 				<span class="post-crumb-category">Health</span>
 				<span class="post-crumb-datetime">July 15th, 2017</span>
@@ -44,12 +45,13 @@
 					<img src="#">
 				</a>
 			</div>
-		</div>
+		</article>
 	</header>
 
+	<!-- Recent Posts -->
 	<section class="section row">
 		<h2 class="section-title col-lg-12">Section Title</h2>
-		<div class="post-medium col-lg-3">
+		<article class="post-medium col-lg-3">
 			<div class="post-image-container">
 				<a href="#{post link}" class="post-image" alt="{post title}">
 					<img src="#" class="author-slug-image">
@@ -73,14 +75,14 @@
 				<span class="post-crumb-datetime">July 15th, 2017</span>
 
 			</div>
-		</div>
-		
+		</article>
 	</section>
 
 
+	<!-- sponsored post section -->
 	<section class="section row">
 
-		<div class="post-medium col-lg-8">
+		<article class="post-large col-lg-8">
 
 			<div class="post-image-container">
 				<span class="post-image-slug top">
@@ -106,7 +108,7 @@
 
 			</div>
 
-		</div>
+		</article>
 
 		<aside class="col-lg-4">
 			<figure class="subscribe-block">
@@ -121,12 +123,12 @@
 			<ul class="recommended-posts">
 				<li class="post-listing">
 					<div class="row">
-						<div class="post-image-container">
+						<div class="post-image-container col-lg-3">
 							<a href="#{post link}" class="post-image" alt="{post title}">
 								<img src="#">
 							</a>
 						</div>
-						<div class="post-details">
+						<div class="post-details col-lg-9">
 							<a href="#" class="post-title-medium">
 								<h3>Sponsored Post Title</h3>
 							</a>
@@ -136,8 +138,153 @@
 				</li>
 			</ul>
 		</aside>
+	</section>
+
+
+	<!-- large post section -->
+	<section class="section row">
+
+		<article class="post-large col-lg-6">
+
+			<div class="post-image-container">
+				<span class="post-image-slug top">
+					<img src="#">
+				</span>
+				<a href="#{post link}" class="post-image" alt="{post title}">
+					<img src="#">
+				</a>
+			</div>
+			<div class="post-details">
+				<a href="#" class="post-title-medium">
+					<h3>Large Post 1</h3>
+				</a>
+
+				<div class="post-author-slug-medium">
+					<img src="#" class="author-slug-image">
+					<span class="author-slug-title">
+						<em>by</em> John Nolan
+					</span>
+				</div>
+
+				<span class="post-crumb-datetime">July 15th, 2017</span>
+
+			</div>
+
+		</article>
+
+		<article class="post-large col-lg-6">
+
+			<div class="post-image-container">
+				<span class="post-image-slug top">
+					<img src="#">
+				</span>
+				<a href="#{post link}" class="post-image" alt="{post title}">
+					<img src="#">
+				</a>
+			</div>
+			<div class="post-details">
+				<a href="#" class="post-title-medium">
+					<h3>Large Post 1</h3>
+				</a>
+
+				<div class="post-author-slug-medium">
+					<img src="#" class="author-slug-image">
+					<span class="author-slug-title">
+						<em>by</em> John Nolan
+					</span>
+				</div>
+
+				<span class="post-crumb-datetime">July 15th, 2017</span>
+
+			</div>
+
+		</article>
+	</section>
+
+
+	<!-- top trending -->
+	<section class="section top-trending row">
+
+		<div class="col-lg-6">
+			<ul class="trending-posts">
+				<li class="post-trending">
+					<div class="row">
+						<div class="post-image-container col-lg-4">
+							<a href="#{post link}" class="post-image" alt="{post title}">
+								<img src="#">
+							</a>
+						</div>
+						<div class="post-details col-lg-8">
+							<a href="#" class="post-title-medium">
+								<h3>Sponsored Post Title</h3>
+							</a>
+							<span class="post-crumb-datetime">July 5th 2017</span>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+
+		<div class="col-lg-6">
+			<ul class="trending-posts">
+				<li class="post-trending">
+					<div class="row">
+						<div class="post-image-container col-lg-4">
+							<a href="#{post link}" class="post-image" alt="{post title}">
+								<img src="#">
+							</a>
+						</div>
+						<div class="post-details col-lg-8">
+							<a href="#" class="post-title-medium">
+								<h3>Sponsored Post Title</h3>
+							</a>
+							<span class="post-crumb-datetime">July 5th 2017</span>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
 		
 	</section>
+
+
+	<!-- Inspiring Posts -->
+	<section class="section row">
+		<h2 class="section-title col-lg-12">Inspiring Posts</h2>
+		<div class="col-lg-9">
+			<div class="posts">
+				<article class="post-small col-lg-4">
+					<div class="post-image-container">
+						<a href="#{post link}" class="post-image" alt="{post title}">
+							<img src="#" class="author-slug-image">
+						</a>
+					</div>
+					<div class="post-details">
+						<a href="#" class="post-title-medium">
+							<h3>8 Ways to Refresh and Personalize Your Home</h3>
+						</a>
+
+						<div class="post-author-slug-medium">
+							<img src="#" class="author-slug-image">
+							<span class="author-slug-title">
+								<em>by</em> John Nolan
+							</span>
+						</div>
+
+						<span class="post-crumb-datetime">July 15th, 2017</span>
+					</div>
+				</article>
+			</div>
+		</div>
+		<div class="col-lg-3">
+			<figure class="ad-small">
+				Ad
+			</figure>
+		</div>
+	</section>
+
+
+	<?php echo file_get_contents("./src/modules/_footer.php") ?>
 
 
 	<!-- SCRIPTS -->
---
+++ src/modules/_footer.php
@@ -0,0 +1,38 @@
+<footer>
+	<div class="footer-brands row">
+		<a href="#" class="footer-main-logo col-lg-4">
+			<img src="#">
+		</a>
+		<ul class="brands-list col-lg-6">
+			<li>
+				<a href="#" class="brand-logo dim">
+					<img src="#">
+				</a>
+			</li>
+		</ul>
+	</div>
+
+	<div class="footer-legal-links row">
+		<ul class="links-list col-lg-6">
+			<li>
+				<a href="#" class="legal-link">About</a>
+			</li>
+			<li>
+				<a href="#" class="legal-link">Privacy Policy</a>
+			</li>
+			<li>
+				<a href="#" class="legal-link">Terms Of Use</a>
+			</li>
+			<li>
+				<a href="#" class="legal-link">DMCA</a>
+			</li>
+			<li>
+				<a href="#" class="legal-link">Contact</a>
+			</li>
+		</ul>
+		<a href="#" class="footer-fb-logo col-xs-offset-4 col-lg-2">
+			<img src="#">
+		</a>
+	</div>
+	
+</footer>
